2fc48ef5f4ecb1ff727b49668da864781054ab4b
bega <begmuhommet.jumayev@gmail.com>
2024-11-19 09:05:04 +0800

    added recaptcha to the contact form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ const RootLayout = (props: TProps) => {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+
       </body>
     </html>
   );
