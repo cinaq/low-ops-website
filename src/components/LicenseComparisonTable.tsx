ca2a8c74c4d477b3e9c50825c7148f1b44729c51
Xiwen Cheng <x@cinaq.com>
2025-06-28 11:46:58 +0200

    Update pricing

--- src/components/LicenseComparisonTable.tsx
+++ src/components/LicenseComparisonTable.tsx
@@ -41,23 +41,33 @@ const LicenseComparisonTable = () => {
             className="flex flex-col justify-between gap-1 items-stretch h-full px-6"
           >
             <div className="w-full">
-              <h3 className="flex items-center gap-4 text-md font-normal border-b mb-5 pb-2 w-full">
+              <h3 className="flex gap-4 text-md font-normal border-b mb-5 pb-2 w-full">
                 {plan.label}
                 {plan.isPopular && (
                   <Badge variant="primaryOutline">Popular</Badge>
                 )}
               </h3>
             </div>
-            <h1 className="text-3xl font-bold mb-3">{plan.price}</h1>
-            {plan.subPrice && (
-              <p className="font-light text-md text-gray-600 mb-3">
-                {plan.subPrice}
-              </p>
+
+            {plan.free ? (
+              <>
+                <h1 className="text-3xl font-bold mb-3"><span className="line-through">{plan.price}</span> Free</h1>
+                <p className="font-light text-md text-gray-600 mb-3">
+                  Limited time offer. Free for first year.
+                </p>
+              </>
+            ) : (
+              <>
+                <h1 className="text-3xl font-bold mb-3">{plan.price}</h1>
+                <p className="font-light text-md text-gray-600 mb-3">
+                  {' '}
+                </p>
+              </>
             )}
             <p className="font-light text-md text-gray-600 mb-3">
               {plan.description}
             </p>
-            { plan.link ? (
+            {plan.link ? (
               <a href={plan.link}>
                 <Button className="mb-2">Install Now</Button>
               </a>
@@ -77,7 +87,7 @@ const LicenseComparisonTable = () => {
       <Fragment key={key}>
         <div
           className={cn(
-            'font-normal border-b py-3 border-gray-100 text-gray-700 pl-2',
+            'font-bold border-b py-3 border-gray-100 text-gray-700 pl-2',
             index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
           )}
         >
@@ -87,14 +97,14 @@ const LicenseComparisonTable = () => {
           const value = plan.items[key as keyof typeof plan.items] || '-';
           const resultValue =
             valuesWithIcon[
-              value.toLowerCase() as keyof typeof valuesWithIcon
+            value.toLowerCase() as keyof typeof valuesWithIcon
             ] ?? value;
 
           return (
             <div
               key={`${plan.value}-${key}`}
               className={cn(
-                'text-center border-b py-3 border-gray-100 text-gray-900 font-medium flex items-center justify-center gap-2',
+                'border-b py-3 border-gray-100 text-gray-900 font-medium flex gap-2 pl-6',
                 index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
               )}
             >
@@ -110,50 +120,6 @@ const LicenseComparisonTable = () => {
     <div className="flex flex-col gap-8">
       <div className="hidden lg:grid grid-cols-4">{renderPlans()}</div>
       <div className="hidden lg:grid grid-cols-4">{renderItems()}</div>
-      <div className="flex flex-col gap-20 lg:hidden">
-        {plans.map((plan, index) => (
-          <div key={index} className="flex flex-col gap-6">
-            <div className="flex flex-col justify-between gap-1 items-stretch h-full">
-              <div className="w-full">
-                <h3 className="flex items-center gap-4 text-md font-normal border-b mb-5 pb-2 w-full">
-                  {plan.label}
-                  {plan.isPopular && (
-                    <Badge variant="primaryOutline">Popular</Badge>
-                  )}
-                </h3>
-              </div>
-              <h1 className="text-3xl font-bold mb-3">{plan.price}</h1>
-              <p className="font-light text-md text-gray-600 mb-3">
-                {plan.description}
-              </p>
-              <Button className="mb-2 max-w-[200px]">Get Started</Button>
-            </div>
-            <div className="">
-              {Object.entries(plan.items).map(([key, value], i) => (
-                <div
-                  key={`${key}-${plan.value}`}
-                  className={cn(
-                    'flex items-center justify-between p-3 border-b border-gray-100',
-                    i % 2 === 0 ? 'bg-gray-50' : 'bg-white'
-                  )}
-                >
-                  <div className="text-gray-700 font-light">
-                    {capitalize(key)}
-                  </div>
-                  <div
-                    key={`${plan.value}-${key}`}
-                    className="text-gray-900 font-medium flex items-center justify-center gap-2"
-                  >
-                    {valuesWithIcon[
-                      value.toLowerCase() as keyof typeof valuesWithIcon
-                    ] ?? value}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
