f4dde4dff0cee8d2e5aa63b5ff3d064ababebc8a
Xiwen Cheng <x@cinaq.com>
2026-06-12 16:05:12 +0200

    fix

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import RedirectPage from '@/components/RedirectPage';
+
+const NotFound = () => {
+  return <RedirectPage />;
+};
+
+export default NotFound;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import RedirectPage from '@/components/RedirectPage';
+
+const Home = () => {
+  return <RedirectPage />;
+};
+
+export default Home;
